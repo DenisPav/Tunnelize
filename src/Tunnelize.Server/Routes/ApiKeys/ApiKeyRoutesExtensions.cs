d983d82831b06e2dc5755c8bcba665a091d5e96d
Denis Pavlović <denis.pav@hotmail.com>
2024-05-21 21:38:49 +0200

    Add initial creation of api keys

--- src/Tunnelize.Server/Routes/ApiKeys/ApiKeyRoutesExtensions.cs
+++ src/Tunnelize.Server/Routes/ApiKeys/ApiKeyRoutesExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
+using Tunnelize.Server.Persistence;
+using Tunnelize.Server.Persistence.Entities;
 
 namespace Tunnelize.Server.Routes.ApiKeys;
 
@@ -15,19 +17,30 @@ public static class ApiKeyRoutesExtensions
             return TypedResults.Empty;
         });
 
-        apiKeysGroup.MapPost("/create", (
+        apiKeysGroup.MapPost("/create", async (
             [FromForm] CreateApiKeyRequest request,
             IValidator<CreateApiKeyRequest> validator,
+            DatabaseContext db,
+            CancellationToken cancellationToken,
             HttpContext context) =>
         {
-            var validationResult = validator.Validate(request);
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
             if (validationResult.IsValid == false)
                 return Results.BadRequest();
+
+            var entity = new ApiKey
+            {
+                SubDomain = request.Subdomain
+            };
+            
+            //TODO: add validation that this doesn't exist and add unique index to DB
+            await db.Set<ApiKey>().AddAsync(entity, cancellationToken);
+            await db.SaveChangesAsync(cancellationToken);
             
             context.Response.Headers.Append("HX-Redirect", "/dashboard");
             return Results.Empty;
         }).DisableAntiforgery();
 
-        apiKeysGroup.MapDelete("/delete/{id}", () => { return TypedResults.Empty; });
+        apiKeysGroup.MapDelete("/delete/{id}", () => TypedResults.Empty);
     }
 }
